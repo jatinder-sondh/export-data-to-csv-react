b0f7b172a50277c82be0efc2dfd4b9783f5c5d02
Jatinder Kaur <jatinder.kaur@kochartech.com>
2022-09-12 16:47:02 +0530

    typo

--- src/App.js
+++ src/App.js
@@ -52,48 +52,6 @@ function App() {
       email: "dycubo@mailinator.com",
       age: "87",
     },
-    {
-      firstName: "Warren",
-      lastName: "Morrow",
-      email: "sokyt@mailinator.com",
-      age: "36",
-    },
-    {
-      firstName: "Gwendolyn",
-      lastName: "Galloway",
-      email: "weciz@mailinator.com",
-      age: "76",
-    },
-    {
-      firstName: "Astra",
-      lastName: "Wyatt",
-      email: "quvyn@mailinator.com",
-      age: "57",
-    },
-    {
-      firstName: "Jasmine",
-      lastName: "Wong",
-      email: "toxazoc@mailinator.com",
-      age: "42",
-    },
-    {
-      firstName: "Brooke",
-      lastName: "Mcconnell",
-      email: "vyry@mailinator.com",
-      age: "56",
-    },
-    {
-      firstName: "Christen",
-      lastName: "Haney",
-      email: "pagevolal@mailinator.com",
-      age: "23",
-    },
-    {
-      firstName: "Tate",
-      lastName: "Vega",
-      email: "dycubo@mailinator.com",
-      age: "87",
-    },
   ]);
 
   const [firstName, setFirstName] = useState("Lucifer");
